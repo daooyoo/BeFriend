e210efd16d7cab6b2e31042b825b1bffc665cd6e
David Yoo <David@davids-macbook-pro.local>
2016-06-09 14:38:27 -0500

    removed nav controller and set up basic sign up and login pages with error handling

--- BeFriend/SignInViewController.swift
+++ BeFriend/SignInViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    enum Error: ErrorType {
+        case NoUsername
+        case NoPassword
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        setup()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,5 +31,56 @@ class SignInViewController: UIViewController {
     
     @IBAction func cancelSignIn() {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func setup() {
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == usernameTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    @IBAction func loginToAccount() {
+        if checkForBlankFields() == false {
+            showAlert("Welcome Back, \(usernameTextField.text!)!")
+        }
+    }
+    
+    func checkForBlankFields() -> Bool {
+        do {
+            
+            if usernameTextField.text == "" {
+                throw Error.NoUsername
+            } else if passwordTextField.text == "" {
+                throw Error.NoPassword
+            }
+            
+        } catch Error.NoUsername {
+            showAlert("Please Enter Your Username")
+            return true
+        } catch Error.NoPassword {
+            showAlert("Please Enter Your Password")
+            return true
+        } catch let error {
+            fatalError("\(error)")
+        }
+        return false
+    }
+
+    func showAlert(title: String, message: String? = nil, style: UIAlertControllerStyle = .Alert) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        
+        alertController.addAction(okAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
     }
 }
--- BeFriend/ViewController.swift
+++ BeFriend/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  BeFriend
 //
-//  Created by David Yoo on 6/6/16.
+//  Created by David Yoo on 6/7/16.
 //  Copyright © 2016 David Yoo. All rights reserved.
 //
 
@@ -12,12 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let nav = self.navigationController?.navigationBar
-        nav?.translucent = false
-        nav?.shadowImage = UIImage()
-        nav?.setBackgroundImage(UIImage(), forBarMetrics: .Default)
-        nav?.barTintColor = UIColor(red: 234/255, green: 208/255, blue: 45/255, alpha: 1.0)
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +21,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
